3644042c6904fcd45fb312acbb8a7593f373f98d
Anwesh43 <anweshthecool0@gmail.com>
2017-08-17 17:13:56 +0530

    adding menu router component

--- menu-router-component.js
+++ menu-router-component.js
@@ -9,9 +9,28 @@ class MenuRouterComponent extends Component {
         this.div.style.width = w/3
     }
     render() {
-        
+
     }
     componentDidMount() {
+        this.render()
+    }
+}
+class MenuComponent extends HTMLElement{
+    constructor() {
+        super()
+        this.img = document.createElement('img')
+        const shadow = this.attachShadow({mode:'open'})
+        this.text = this.innerHTML
+        shadow.appendChild(this.img)
+    }
+    render() {
+        const canvas = document.createElement('canvas')
+        canvas.width = w/3
+        canvas.height = h/12
+        const context = canvas.getContext('2d')
+        this.img.src = canvas.toDataURL()
+    }
+    connectedCallback() {
         this.render()
     }
 }
